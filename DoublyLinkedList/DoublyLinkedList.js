d9bcb1f9dfdc3cb5fda9e635a786e6509a9beb55
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-04-08 21:45:38 -0400

    Pop and shift

--- DoublyLinkedList/DoublyLinkedList.js
+++ DoublyLinkedList/DoublyLinkedList.js
@@ -22,11 +22,40 @@ class DoublyLinkedList {
         }
         this.length++;
         return this;
-
+    }
+    pop(){
+        if(this.length === 0) return undefined;
+        else{
+            var deletedTail = this.tail;
+            if(this.length === 1){ this.head = null; this.tail = null}
+            else{
+                this.tail = deletedTail.prev;
+                this.tail.next = null;    
+                deletedTail.prev = null;         
+            }
+            this.length--;
+            return deletedTail;
+        }
+    }
+    shift(){
+        if(this.length === 0 ) return undefined;
+        else{
+            var oldHead = this.head;
+            if(this.length === 1) {this.head = null; this.tail = null}
+            else{
+                this.head = oldHead.next;
+                this.head.prev = null;
+                oldHead.next = null
+            }
+            this.length--;
+            return oldHead 
+        }
     }
 }
 
 var list = new DoublyLinkedList(10);
 list.push(10);
 list.push(20);
+list.push(30);
+list.pop();
 console.log(list);
