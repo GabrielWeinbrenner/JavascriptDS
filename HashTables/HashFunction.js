8db2f993d8b128629289a72b1dc8e03835e734cd
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-04-16 19:32:58 -0400

    sfowf

--- HashTables/HashFunction.js
+++ HashTables/HashFunction.js
@@ -6,5 +6,14 @@ function hash(key,arrayLength){
     }
     return total;
 }
-
+function refactoredHash(key,arrayLength){
+    let total = 0;
+    let WEIRD_PRIME = 31;
+    for(let i = 0; i < Math.min(key.length,100); i++){
+        let char = key[i];
+        let value = char.charCodeAt(0) -96;
+        total = (total * WEIRD_PRIME + value) % arrayLength;
+    }
+    return total;
+}
 console.log(hash("hello"));
