2450eb5e846a574379a5a2fe181721b2dc69fed4
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-04-15 20:03:44 -0400

    AIhfowq

--- BinaryHeaps/MaxBinaryHeap.js
+++ BinaryHeaps/MaxBinaryHeap.js
@@ -46,10 +46,10 @@ class MaxBinaryHeap{
     removeRefactored(){
         const max = this.values[0];
         const end = this.values.pop();
-        this.values[0] = end;
-
-        this.sinkDown();
-
+        if(this.values.length > 0){
+            this.values[0] = end;
+            this.sinkDown();
+        }
         return max;
     }
     sinkDown(){
