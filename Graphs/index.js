4829a208a1c4331bea9e9fc606b0a162747253a6
Gabriel Weinbrenner <angelgabe101@gmail.com>
2020-04-26 23:27:16 -0400

    Addedge

--- Graphs/index.js
+++ Graphs/index.js
@@ -3,6 +3,17 @@ class Graph {
         this.adjacencyList = {};
     }
     addVertex(v){
-        this.adjacencyList[v] = [];
+        if(!this.adjacencyList[v]) this.adjacencyList[v] = [];
+    }
+    addEdge(v1,v2){
+        this.adjacencyList[v1].push(v2);
+        this.adjacencyList[v2].push(v1);
     }
 }
+
+let g = new Graph()
+g.addVertex("New York");
+g.addVertex("Tokyo");
+g.addVertex("Los Angeles");
+
+g.addEdge("New York", "Tokyo");
